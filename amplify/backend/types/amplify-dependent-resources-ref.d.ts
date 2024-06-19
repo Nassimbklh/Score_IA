46f8da64334adbd424582736d3d3f10d4a4dd4c0
Nassim Barkallah <75304726+Nassimbklh@users.noreply.github.com>
2024-06-19 14:01:42 +0200

    initialisation projet

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifynextjs5f1eaeeb": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
